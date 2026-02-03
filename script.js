4d0dbae5fbb240eeb24418453efa8d6745c2ddae
Khalid Abdi <abdulrahman.khalid@hotmail.com>
2026-02-03 13:19:50 +0300

    Implement calculateAkanName function to retrieve user input for name generation

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const akanNames = {
   female: ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 };
 
+// Logic
+function calculateAkanName() {
+  const dd = parseInt(document.querySelector('[data-input="day"]').value);
+  const mm = parseInt(document.querySelector('[data-input="month"]').value);
+  const yearInput = document.querySelector('[data-input="year"]').value;
+  const gender = document.querySelector('[data-input="gender"]').value;
+  const resultDisplay = document.querySelector('[data-ref="result"]');
+
+}
